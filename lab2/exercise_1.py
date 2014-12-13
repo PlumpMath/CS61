afa89e35bf2c41a4fc5afe914a2a03e7794136e4
Jason Leaster <jasonleaster@gmail.com>
2014-12-13 17:18:30 +0800

    new file:   lab2/CS 61A Fall 2011_ Lab 2.pdf 	new file:   lab2/exercise_1.py

---
+++ lab2/exercise_1.py
@@ -0,0 +1,15 @@
+"""
+Code Writer : EOF
+Code file   : exercise_1.py
+Code date   : 2014.12.13
+
+
+Attention here. 
+      Every new line can not start by a blank.
+"""
+
+a = 1
+b = a + 1
+a + 1 + a*b
+
+print a,b
